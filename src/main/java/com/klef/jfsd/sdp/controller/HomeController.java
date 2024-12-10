ad6fbeac3a6a4e66f73fae3ffa3e40a7033ac306
Gnanasai1234 <115910766+Gnanasai1234@users.noreply.github.com>
2024-12-10 20:00:02 +0530

    Update HomeController.java

--- src/main/java/com/klef/jfsd/sdp/controller/HomeController.java
+++ src/main/java/com/klef/jfsd/sdp/controller/HomeController.java
@@ -1,1 +1,26 @@
 
+package com.klef.jfsd.sdp.controller;
+
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+	
+	
+	
+	@GetMapping("/")
+	public String home()
+	{
+		return "Your Backend is Running...";
+	}
+	
+	
+	
+
+}
